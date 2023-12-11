25db0eed98253406c2cf4ca78b496c4dd30452f8
MSundarAI <69122399+MSundarAI@users.noreply.github.com>
2023-12-11 20:05:51 +0000

    added last definition

--- Part2/pages/ca_collab_background.py
+++ Part2/pages/ca_collab_background.py
@@ -107,5 +107,5 @@ These features were collected from Spotify, for further explainability, see belo
 - **instrumentalness**: Predicts whether a track contains no vocals. "Ooh" and "aah" sounds are treated as instrumental in this context. Rap or spoken word tracks are clearly "vocal". The closer the instrumentalness value is to 1.0, the greater likelihood the track contains no vocal content. Values above 0.5 are intended to represent instrumental tracks, but confidence is higher as the value approaches 1.0.
 - **liveness**: Detects the presence of an audience in the recording. Higher liveness values represent an increased probability that the track was performed live. A value above 0.8 provides strong likelihood that the track is live.
 - **loudness**: The overall loudness of a track in decibels (dB). Loudness values are averaged across the entire track and are useful for comparing relative loudness of tracks. Loudness is the quality of a sound that is the primary psychological correlate of physical strength (amplitude). Values typically range between -60 and 0 db.
-- **duration_ms**: The duration of the track in milliseconds.
+- **tempo**: The overall estimated tempo of a track in beats per minute (BPM). In musical terminology, tempo is the speed or pace of a given piece and derives directly from the average beat duration.
 """)
