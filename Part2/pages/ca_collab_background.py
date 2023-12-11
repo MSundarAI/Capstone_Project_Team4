697706906c5cbce44bb261cfced45586904b555d
MSundarAI <69122399+MSundarAI@users.noreply.github.com>
2023-12-11 14:36:16 +0000

    adding import

--- Part2/pages/ca_collab_background.py
+++ Part2/pages/ca_collab_background.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import os
 st.set_page_config(page_title="background", page_icon="🎹", layout="wide")
 st.image('images/logo.png', caption = None)
 st.write("Current working directory:", os.getcwd())
